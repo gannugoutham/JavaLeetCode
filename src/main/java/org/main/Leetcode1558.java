80d7a971979efcd3ccdcd60868ff41e90aebdf6a
gannugoutham <gannugoutham26@gmail.com>
2023-03-02 10:26:26 -0600

    leetcode-1558-java-arrays

--- src/main/java/org/main/Leetcode1558.java
+++ src/main/java/org/main/Leetcode1558.java
@@ -1,5 +1,28 @@
 package org.main;
 
-public class Leetcode1558 {
+import java.util.Arrays;
 
+public class Leetcode1558 {
+    private int[] arr;
+//constructor
+    public Leetcode1558(int[] array) {
+        this.arr = array;
+    }
+    public void minCount() {
+        int arrLength = arr.length, addCount = 0, mulCount = 0;
+        for(int i=0; i<arrLength; i++) {
+            int currMul = 0;
+            while(arr[i]>0) {
+                if(arr[i]%2 !=0 ) {
+                    arr[i]--;
+                    addCount++;
+                } else {
+                    arr[i] /= 2;
+                    currMul++;
+                }
+            }
+            mulCount = Math.max(mulCount, currMul);
+        }
+        System.out.println(addCount + mulCount);
+    }
 }
--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -2,6 +2,11 @@ package org.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello2 world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world1!");
+        int[] arr = {4,2};
+        Leetcode1558 leetcode1558 = new Leetcode1558(arr);
+        leetcode1558.minCount();
+        System.out.println("here");
     }
 }
